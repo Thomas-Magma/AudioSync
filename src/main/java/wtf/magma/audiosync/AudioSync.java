847ef88b72de9e9513f8cdf94d87079d26093850
magma66666 <bhg12311154@163.com>
2025-11-09 16:03:26 +0800

    添加系统检测

--- src/main/java/wtf/magma/audiosync/AudioSync.java
+++ src/main/java/wtf/magma/audiosync/AudioSync.java
@@ -1,18 +1,28 @@
 package wtf.magma.audiosync;
 
+import com.mojang.logging.LogUtils;
 import lombok.Getter;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Util;
+import org.slf4j.Logger;
 import wtf.magma.audiosync.audio.HUD;
 import wtf.magma.audiosync.audio.SMTC;
 import wtf.magma.audiosync.event.EventManager;
 
+@Getter
 public class AudioSync implements ModInitializer {
     public static AudioSync Instance = new AudioSync();
-    @Getter
     private EventManager eventManager = new EventManager();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     @Override
     public void onInitialize() {
+        if (!(Util.getOperatingSystem() == Util.OperatingSystem.WINDOWS)) {
+            LOGGER.error("Audio Sync can't running on {}", Util.getOperatingSystem());
+            return;
+        }
+
         SMTC.thread();
         new HUD();
     }
